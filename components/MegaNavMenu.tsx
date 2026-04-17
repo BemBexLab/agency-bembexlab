084734ff8489c188f019f486c714e71f9a6ae428
car5p3 <triggred47@icloud.com>
2026-04-17 15:57:19 -0700

    Updated mega nav

--- components/MegaNavMenu.tsx
+++ components/MegaNavMenu.tsx
@@ -7,68 +7,113 @@ import { IoIosArrowDropright } from "react-icons/io";
 const serviceCategories = [
     {
         title: { label: "Web Design & Development", href: "/services/web-design-development/" },
-        items: [
-            { label: "Custom Website Design", href: "/services/web-design-development/custom-web-design-and-development" },
-            { label: "E-commerce Website\nDevelopment", href: "/services/web-design-development/ecommerce-website-development" },
-            { label: "Magento Development", href: "/services/web-design-development/magento-development" },
-            { label: "Shopify Development", href: "/services/web-design-development/shopify-development" },
-            { label: "WooCommerce Development", href: "/services/web-design-development/woocommerce-development" },
-            { label: "Wordpress Development", href: "/services/web-design-development/wordpress-development" },
-        ]
+        // items: [
+        //     { label: "Custom Website Design", href: "/services/web-design-development/custom-web-design-and-development" },
+        //     { label: "E-commerce Website\nDevelopment", href: "/services/web-design-development/ecommerce-website-development" },
+        //     { label: "Magento Development", href: "/services/web-design-development/magento-development" },
+        //     { label: "Shopify Development", href: "/services/web-design-development/shopify-development" },
+        //     { label: "WooCommerce Development", href: "/services/web-design-development/woocommerce-development" },
+        //     { label: "Wordpress Development", href: "/services/web-design-development/wordpress-development" },
+        // ]
     },
     {
-        title: { label: "Search Engine Optimization", href: "/services/search-engine-optimization" },
-        items: [
-            { label: "SEO Strategy & Planning", href: "/services/search-engine-optimization/seo-strategy-planning" },
-            { label: "Local SEO Service", href: "/services/search-engine-optimization/local-seo-service" },
-            // { label: "Local Listing", href: "/services/search-engine-optimization/local-listing" },
-            { label: "On-page & Off-page", href: "/services/search-engine-optimization/onpage-offpage-seo" },
-            { label: "Link Building", href: "/services/search-engine-optimization/link-building" },
-            { label: "Technical SEO Infrastructure", href: "/services/search-engine-optimization/technical-seo-infrastructure" },
-            { label: "Google Profile Optimization", href: "/services/search-engine-optimization/google-profile-optimization" },
-        ]
+        title: { label: "Graphic Design", href: "/services/graphic-design" },
+        // items: [
+        //     { label: "SEO Strategy & Planning", href: "/services/search-engine-optimization/seo-strategy-planning" },
+        //     { label: "Local SEO Service", href: "/services/search-engine-optimization/local-seo-service" },
+        //     // { label: "Local Listing", href: "/services/search-engine-optimization/local-listing" },
+        //     { label: "On-page & Off-page", href: "/services/search-engine-optimization/onpage-offpage-seo" },
+        //     { label: "Link Building", href: "/services/search-engine-optimization/link-building" },
+        //     { label: "Technical SEO Infrastructure", href: "/services/search-engine-optimization/technical-seo-infrastructure" },
+        //     { label: "Google Profile Optimization", href: "/services/search-engine-optimization/google-profile-optimization" },
+        // ]
     },
     {
-        title: { label: "Performance Marketing", href: "/services/performance-marketing" },
-        items: [
-            { label: "Google Ads", href: "/services/performance-marketing/google-ads" },
-            { label: "Youtube Ads", href: "/services/performance-marketing/youtube-ads" },
-            { label: "Meta Ads", href: "/services/performance-marketing/meta-ads" },
-            { label: "Tiktok Shop Ads", href: "/services/performance-marketing/tiktok-shop-ads" },
-            { label: "Linkedin Ads", href: "/services/performance-marketing/linkedin-ads" },
-            { label: "Shopping Ads", href: "/services/performance-marketing/shopping-ads" },
-            { label: "Affiliate Marketing", href: "/services/performance-marketing/affiliate-marketing" },
-            { label: "Influencer Marketing", href: "/services/performance-marketing/influencer-marketing" },
-            { label: "App Marketing", href: "/services/performance-marketing/app-marketing" },
-        ]
+        title: { label: "Video Editing", href: "/services/video-editing" },
+        // items: [
+        //     { label: "Google Ads", href: "/services/performance-marketing/google-ads" },
+        //     { label: "Youtube Ads", href: "/services/performance-marketing/youtube-ads" },
+        //     { label: "Meta Ads", href: "/services/performance-marketing/meta-ads" },
+        //     { label: "Tiktok Shop Ads", href: "/services/performance-marketing/tiktok-shop-ads" },
+        //     { label: "Linkedin Ads", href: "/services/performance-marketing/linkedin-ads" },
+        //     { label: "Shopping Ads", href: "/services/performance-marketing/shopping-ads" },
+        //     { label: "Affiliate Marketing", href: "/services/performance-marketing/affiliate-marketing" },
+        //     { label: "Influencer Marketing", href: "/services/performance-marketing/influencer-marketing" },
+        //     { label: "App Marketing", href: "/services/performance-marketing/app-marketing" },
+        // ]
     },
     {
-        title: { label: "Logo Design & Branding", href: "/services/logo-design-branding" },
-        items: [
-            { label: "Logo Design &\nVisual Systems", href: "/services/logo-design-branding/logo-design-visual-systems" },
-            { label: "Brand Guide Book", href: "/services/logo-design-branding/brand-guide-book" },
-            { label: "Personal Branding", href: "/services/logo-design-branding/personal-branding" },
-            { label: "Video & Motion Graphics", href: "/services/logo-design-branding/video-motion-graphics" },
-            { label: "Brand Development", href: "/services/logo-design-branding/brand-development" },
-        ]
+        title: { label: "Mobile Development", href: "/services/mobile-development" },
+        // items: [
+        //     { label: "Logo Design &\nVisual Systems", href: "/services/logo-design-branding/logo-design-visual-systems" },
+        //     { label: "Brand Guide Book", href: "/services/logo-design-branding/brand-guide-book" },
+        //     { label: "Personal Branding", href: "/services/logo-design-branding/personal-branding" },
+        //     { label: "Video & Motion Graphics", href: "/services/logo-design-branding/video-motion-graphics" },
+        //     { label: "Brand Development", href: "/services/logo-design-branding/brand-development" },
+        // ]
     },
     {
-        title: { label: "Social Media Marketing &\nManagement", href: "/services/socialmediamarketing-management" },
-        items: [
-            { label: "Profile Optimization", href: "/services/socialmediamarketing-management/profile-optimization" },
-            { label: "High-Impact Content\nSystems", href: "/services/socialmediamarketing-management/high-impact-content-systems" },
-            { label: "Conversion-Focused\nProfile Architecture", href: "/services/socialmediamarketing-management/conversion-focused-profile-architecture" },
-            { label: "Trend Intelligence &\nReach Amplification", href: "/services/socialmediamarketing-management/trend-intelligence-reach-amplification" },
-        ]
+        title: { label: "Web Development", href: "/services/web-development" },
+        // items: [
+        //     { label: "Profile Optimization", href: "/services/socialmediamarketing-management/profile-optimization" },
+        //     { label: "High-Impact Content\nSystems", href: "/services/socialmediamarketing-management/high-impact-content-systems" },
+        //     { label: "Conversion-Focused\nProfile Architecture", href: "/services/socialmediamarketing-management/conversion-focused-profile-architecture" },
+        //     { label: "Trend Intelligence &\nReach Amplification", href: "/services/socialmediamarketing-management/trend-intelligence-reach-amplification" },
+        // ]
     },
     {
-        title: { label: "Content Marketing", href: "/services/content-marketing" },
-        items: [
-            { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
-            { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
-            { label: "Copywriting", href: "/services/content-marketing/copywriting" },
-            { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
-        ]
+        title: { label: "Digital Marketing", href: "/services/digital-design" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
+    },
+    {
+        title: { label: "Digital Marketing", href: "/services/digital-marketing" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
+    },
+    {
+        title: { label: "Content Writing", href: "/services/content-writing" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
+    },
+    {
+        title: { label: "Logo Design", href: "/services/logo-design" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
+    },
+    {
+        title: { label: "Branding", href: "/services/branding" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
+    },
+    {
+        title: { label: "SEO", href: "/services/seo" },
+        // items: [
+        //     { label: "Email Marketing", href: "/services/content-marketing/email-marketing" },
+        //     { label: "SEO Content Writing", href: "/services/content-marketing/seo-content-writing" },
+        //     { label: "Copywriting", href: "/services/content-marketing/copywriting" },
+        //     { label: "Conversion Rate\nOptimization (CRO)", href: "/services/content-marketing/conversion-rate-optimization-cro" },
+        // ]
     },
 ];
 
